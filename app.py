bea0b21b1eeb01ae1bb747fa4c674249332e0ccc
QuangVinh1601 <nguyenhuuquangvinh5@gmail.com>
2025-05-24 10:30:50 +0700

    add log

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def get_db_connection():
     """Establishes a connection to the SQL Server database using the provided connection string."""
     try:
         conn = pyodbc.connect(SQL_SERVER_CONNECTION_STRING)
+        print(SQL_SERVER_CONNECTION_STRING)
         return conn
     except pyodbc.Error as ex:
         sqlstate = ex.args[0]
